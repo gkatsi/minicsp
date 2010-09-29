910a1a0ae0f0312e217eca167db186ea0d61ce54
George Katsirelos <gkatsi@gmail.com>
2010-09-29 18:59:04 +0200

    Maintenance mode in fz frontend, does not catch unsat exception

--- fz/main-fz.cpp
+++ fz/main-fz.cpp
@@ -24,18 +24,21 @@ int main(int argc, char *argv[])
 
   cmdline::parse_solver_options(s, args);
   bool stat = cmdline::has_option(args, "--stat");
+  bool maint = cmdline::has_option(args, "--maint");
   setup_signal_handlers(&s);
 
   double cpu_time = cpuTime();
 
   FlatZinc::Printer p;
   FlatZinc::FlatZincModel *fm = 0L;
-  try {
+  if( maint ) // do not catch exceptions
     fm = parse(args.back(), s, p);
-  } catch (unsat& e) {
-    cout << setw(5) << setfill('=') << '='
-         << "UNSATISFIABLE" << setw(5) << '=' << "\n";
-  }
+  else try {
+      fm = parse(args.back(), s, p);
+    } catch (unsat& e) {
+      cout << setw(5) << setfill('=') << '='
+           << "UNSATISFIABLE" << setw(5) << '=' << "\n";
+    }
   if( !fm ) return 0;
 
   double parse_time = cpuTime() - cpu_time;
